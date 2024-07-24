bc3cee9986a1ec8781f29f32c8eda19e5233dfd1
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-07-24 18:06:14 +0530

    add : create post screen content

--- app/src/main/java/com/example/mbmkadhumdhadaka/pages/CreatePost.kt
+++ app/src/main/java/com/example/mbmkadhumdhadaka/pages/CreatePost.kt
@@ -1,9 +1,21 @@
 package com.example.mbmkadhumdhadaka.pages
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
@@ -17,16 +29,28 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextGeometricTransform
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.mbmkadhumdhadaka.R
 import com.example.mbmkadhumdhadaka.viewModel.AuthViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 //@Composable
 fun CreatePost(navController: NavController, authViewModel: AuthViewModel) {
+    var postContent by remember { mutableStateOf("") }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -54,9 +78,36 @@ fun CreatePost(navController: NavController, authViewModel: AuthViewModel) {
                 .fillMaxSize()
                 .padding(innerPadding)
                 .padding(16.dp) // Add any additional padding if needed
+            , horizontalAlignment = Alignment.Start 
         ) {
+          
+            Row {
+                Spacer(modifier = Modifier.width(5.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier
+                        .size(80.dp)
+                        .clip(CircleShape)
+                        .border(1.dp, Color.Gray, CircleShape)
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Text("your_name", style = MaterialTheme.typography.h6)
+            }
 
-
+            Spacer(modifier = Modifier.height(20.dp))
+            BasicTextField(
+                value = postContent,
+                onValueChange = { postContent = it },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                modifier = Modifier.height(100.dp).fillMaxWidth()
+                    .background(Color.Gray.copy(alpha = 0.1f), RectangleShape)
+                    .border(0.dp, Color.Gray,RectangleShape)
+                    .padding(16.dp)
+            )
+            
         }
     }
 }
